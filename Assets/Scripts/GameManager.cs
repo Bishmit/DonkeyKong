cc8cf604a01a148b7ce9d8e562be366c1d3fb4d5
Bishmit <bishmitregmi@gmail.com>
2025-05-06 22:10:37 +0545

    level added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,34 +1,71 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager: MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-   private int lives; 
-   private int score; 
+    private int lives;
+    private int score;
+    private int level;
 
-   private void Start()
-   {
-     NewGame(); 
-   } 
-
-   private void NewGame(){
-      lives = 3; 
-      score = 0; 
-
-      // load level
-   }
-
-   public void LevelComplete(){
-        score += 100; 
-   }
-
-   public void LevelFailed(){
-    lives--; 
-    if(lives <= 0){
-        NewGame(); 
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        NewGame();
     }
-    else{
-        // Reload Level 
 
+    private void NewGame()
+    {
+        lives = 3;
+        score = 0;
+        LoadLevel(1);
     }
-   }
+
+    private void LoadLevel(int index)
+    {
+        level = index;
+
+        Camera camera = Camera.main;
+        if (camera != null)
+        {
+            camera.cullingMask = 0; // Optional fade out
+        }
+
+        Invoke(nameof(LoadScene), 1f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LevelComplete()
+    {
+        score += 100;
+        int nextLevel = level + 1;
+
+        if (nextLevel < SceneManager.sceneCountInBuildSettings)
+        {
+            LoadLevel(nextLevel);
+        }
+        else
+        {
+            Debug.Log("All levels complete! Restarting last level.");
+            LoadLevel(level); // Replay the current (last) level
+        }
+    }
+
+    public void LevelFailed()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            Debug.Log("Game Over! Restarting game.");
+            NewGame();
+        }
+        else
+        {
+            Debug.Log($"Retrying level. Lives left: {lives}");
+            LoadLevel(level); // Reload current level
+        }
+    }
 }
